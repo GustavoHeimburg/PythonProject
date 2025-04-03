558e8dc411ee51ce530fb4b24687ffdeccdb5661
Pythonprojetos <gutogustavogu@gmail.com>
2025-04-03 15:49:04 -0300

    Commit

---
+++ pedrapapeltesoura.py
@@ -0,0 +1,25 @@
+import random
+
+def jogar():
+    opcoes = ["pedra" , "papel" "tesoura"]
+
+    jogador = input("Escolha Pedra - Papel - tesoura: ").lower()
+    if jogador not in opcoes:
+        print("Escolha invalida")
+        return
+
+    pc = random.choice(opcoes)
+
+    print(f"\nVoce escolheu: {jogador}")
+    print(f"O computador escolheu: {pc}\n")
+
+    if jogador == pc:
+        print("Empate")
+    elif (jogador == "pedra" and pc == "tesoura") or \
+         (jogador == "papel" and pc == "pedra") or \
+         (jogador == "tesoura" and pc == "papel"):
+        print("Voce ganhou!!!")
+    else:
+        print("Voce perdeu!")
+
+jogar()
